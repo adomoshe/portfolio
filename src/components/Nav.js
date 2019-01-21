27e9047a1d44d49faf2d6ea4099487e69b441823
Ado Moshe <adoran.moshe@gmail.com>
2019-01-20 23:04:16 -0800

    Navbar functionality

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,26 +3,35 @@ import React, { Component } from 'react';
 class Nav extends Component {
   render() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="#">Ado</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+      <nav class='navbar navbar-expand-lg navbar-dark bg-primary'>
+        <a class='navbar-brand' href='https://ado-portfolio.herokuapp.com/' rel="noopener noreferrer">
+          Ado
+        </a>
+        <button
+          class='navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarText'
+          aria-controls='navbarText'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span class='navbar-toggler-icon' />
         </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <div class='collapse navbar-collapse' id='navbarText'>
+          <ul class='navbar-nav mr-auto'>
+            <li class='nav-item'>
+              <a class='nav-link' href='https://github.com/adomoshe' target='_blank' rel="noopener noreferrer">
+                Github
+              </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
+            <li class='nav-item'>
+              <a class='nav-link' href='https://www.linkedin.com/in/adoranmoshe/' target='_blank' rel="noopener noreferrer">
+                LinkedIn
+              </a>
             </li>
           </ul>
-          <span class="navbar-text">
-            Navbar text with an inline element
-          </span>
+          <span class='navbar-text'>Navbar text with an inline element</span>
         </div>
       </nav>
     );
