4b4024a61256fd27cc4e7fa0cec3ccc21441cd44
Ado Moshe <adoran.moshe@gmail.com>
2019-01-20 22:25:35 -0800

    Deleted mdb

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+    render() {
+      return (
+        <navbar className="App">
+        
+        </navbar>
+      );
+    }
+  }
+  
+  export default Nav;
