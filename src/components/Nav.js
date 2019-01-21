3a5fe2a3065878ee74dc1a7bc1f6283d3935380e
Ado Moshe <adoran.moshe@gmail.com>
2019-01-20 22:54:14 -0800

    working on navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 
 class Nav extends Component {
-    render() {
-      return (
-        <navbar className="App">
-        
-        </navbar>
-      );
-    }
+  render() {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">Ado</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+          </ul>
+          <span class="navbar-text">
+            Navbar text with an inline element
+          </span>
+        </div>
+      </nav>
+    );
   }
-  
-  export default Nav;
+}
+
+export default Nav;
