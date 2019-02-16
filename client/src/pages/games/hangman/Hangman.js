80ebfeef56006b2454762739e7179ad186a2dd6a
Ado Moshe <adoran.moshe@gmail.com>
2019-02-16 11:09:26 -0800

    Minor refactoring

--- client/src/pages/games/hangman/Hangman.js
+++ client/src/pages/games/hangman/Hangman.js
@@ -216,17 +216,17 @@ class Hangman extends Component {
         <br />
         <h2 style={styles.text}>Tarantino Movies Edition</h2>
         <br />
-        <h3 style={styles.text}>{gameOn ? '' : 'Press Space to play!'}</h3>
+        <h3 style={styles.text}>{gameOn ? null : 'Press Space to play!'}</h3>
         <br/>
-        <h3 style={styles.text}>{gameOn ? '' : `Wins: ${wins}`}</h3>
-        <h3 style={styles.text}>{gameOn ? '' : `Losses: ${losses}`}</h3>
+        <h3 style={styles.text}>{gameOn ? null : `Wins: ${wins}`}</h3>
+        <h3 style={styles.text}>{gameOn ? null : `Losses: ${losses}`}</h3>
         <br />
         <h2 style={styles.text}>{this.state.display}</h2>
-        <h3 style={styles.text}>{gameOn ? wordBeingGuessed : ''}</h3>
+        <h3 style={styles.text}>{gameOn ? wordBeingGuessed : null}</h3>
         <br style={styles.text} />
-        <h4 style={styles.text}>{gameOn ? guessedLetters : ''}</h4>
+        <h4 style={styles.text}>{gameOn ? guessedLetters : null}</h4>
         <h4 style={styles.text}>
-          {gameOn ? `Remaining Guesses: ${remainingGuesses}` : ''}
+          {gameOn ? `Remaining Guesses: ${remainingGuesses}` : null}
         </h4>
       </div>
     );
