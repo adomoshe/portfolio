5f8b0fa3995bbce5b9b7829e1206d447b4b0675c
Ado Moshe <adoran.moshe@gmail.com>
2019-07-17 22:17:41 -0700

    update skills

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,36 +1,39 @@
 // Technical skills section on main page
 
-import React from 'react';
-import { Title, ListItem } from './StyledTags';
+import React from "react";
+import { Title, ListItem } from "./StyledTags";
 
 const frontSkillsArr = [
-  'React.js',
-  'JavaScript (ES6)',
-  'HTML5',
-  'CSS3',
-  'Bootstrap',
-  'Material UI',
-  'jQuery'
+  "React.js",
+  "JavaScript (ES6)",
+  "HTML5",
+  "CSS3",
+  "Accessibility",
+  "Bootstrap",
+  "Material UI",
+  "jQuery"
 ];
 
 const backSkillsArr = [
-  'Node.js',
-  'Express.js',
-  'Passport.js',
-  'Authentication',
-  'SQL',
-  'MySQL',
-  'Sequelize.js',
-  'MongoDB',
-  'Firebase',
-  'Heroku'
+  "Node.js",
+  "Express.js",
+  "Passport.js",
+  "Nginx",
+  "JWT",
+  "Authentication",
+  "SQL",
+  "MySQL",
+  "Sequelize.js",
+  "MongoDB",
+  "Firebase",
+  "Heroku"
 ];
 
 const styles = {
   rootDiv: {},
   section: {
-    fontSize: '2rem',
-    fontWeight: '900'
+    fontSize: "2rem",
+    fontWeight: "900"
   },
   p: {}
 };
