26c5af0db8a933c47e055478f9553d6e1c00c12f
Ado Moshe <adomoshe@Ados-MacBook-Air.local>
2022-09-01 22:17:57 -0700

    Trigger

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import './App.css';
-import Nav from './components/Nav';
-import Main from './pages/Main';
-import GamesHome from './pages/games/GamesHome';
+import "./App.css";
+import Nav from "./components/Nav";
+import Main from "./pages/Main";
+import GamesHome from "./pages/games/GamesHome";
 
-import Hangman from './pages/games/hangman/Hangman';
-import Crystal from './pages/games/crystal/Crystal';
-import Trivia from './pages/games/trivia/Trivia';
+import Hangman from "./pages/games/hangman/Hangman";
+import Crystal from "./pages/games/crystal/Crystal";
+import Trivia from "./pages/games/trivia/Trivia";
 
-import BlogHome from './pages/blog/BlogHome';
-import Cuba from './pages/blog/cuba/Cuba';
-import SpringBreak from './pages/blog/study_abroad/SpringBreak';
-import Dublin from './pages/blog/study_abroad/Dublin';
-import Week2 from './pages/blog/study_abroad/Week2';
-import Arrival from './pages/blog/study_abroad/Arrival';
-import Footer from './components/Footer';
+import BlogHome from "./pages/blog/BlogHome";
+import Cuba from "./pages/blog/cuba/Cuba";
+import SpringBreak from "./pages/blog/study_abroad/SpringBreak";
+import Dublin from "./pages/blog/study_abroad/Dublin";
+import Week2 from "./pages/blog/study_abroad/Week2";
+import Arrival from "./pages/blog/study_abroad/Arrival";
+import Footer from "./components/Footer";
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 import {
   faAt,
   faHandPointer,
   faArrowRight,
-  faCode
-} from '@fortawesome/free-solid-svg-icons';
+  faCode,
+} from "@fortawesome/free-solid-svg-icons";
 
 library.add(fab, faAt, faHandPointer, faArrowRight, faCode);
 
@@ -40,7 +40,7 @@ const App = () => (
           <Route
             exact
             path="/blog"
-            render={props => (
+            render={(props) => (
               <div>
                 <BlogHome {...props} />
                 <Footer />
@@ -50,7 +50,7 @@ const App = () => (
           <Route
             exact
             path="/blog/cuba"
-            render={props => (
+            render={(props) => (
               <div>
                 <Cuba {...props} />
                 <Footer />
@@ -60,7 +60,7 @@ const App = () => (
           <Route
             exact
             path="/blog/spring-break"
-            render={props => (
+            render={(props) => (
               <div>
                 <SpringBreak {...props} />
                 <Footer />
@@ -70,7 +70,7 @@ const App = () => (
           <Route
             exact
             path="/blog/dublin"
-            render={props => (
+            render={(props) => (
               <div>
                 <Dublin {...props} />
                 <Footer />
@@ -80,7 +80,7 @@ const App = () => (
           <Route
             exact
             path="/blog/week_2"
-            render={props => (
+            render={(props) => (
               <div>
                 <Week2 {...props} />
                 <Footer />
@@ -90,7 +90,7 @@ const App = () => (
           <Route
             exact
             path="/blog/arrival"
-            render={props => (
+            render={(props) => (
               <div>
                 <Arrival {...props} />
                 <Footer />
