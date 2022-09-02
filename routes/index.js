875f2ecfa8903ddb8969b07fc6f93a84b6915878
Ado Moshe <adomoshe@Ados-MacBook-Air.local>
2022-09-01 22:36:11 -0700

    Added Apple app site association

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,25 @@
-const path = require('path');
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const path = require("path");
+const router = require("express").Router();
+const apiRoutes = require("./api");
 
-router.use('/api', apiRoutes);
+router.use("/api", apiRoutes);
+
+router.use("/apple-app-site-association", (req, res) => {
+  res.json({
+    applinks: {
+      apps: [],
+      details: [
+        {
+          appID: "SVB475XH7W.com.moshe-studios.queue",
+          paths: ["*"],
+        },
+      ],
+    },
+  });
+});
 
 router.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
